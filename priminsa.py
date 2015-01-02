ee528e9827638a3224a4557ac2377585bccdd1c6
adriantora <adriantorax@gmail.com>
2015-01-01 21:27:01 -0500

    redimension de pantalla y velocidades

--- priminsa.py
+++ priminsa.py
@@ -1,7 +1,8 @@
 # !/usr/bin/python
 # -*- coding:utf-8 -*-
 
-#Priminsa es unjuego independiente producido por el SR. José Santos Lertora.
+#Priminsa es unjuego independiente producido por el José Santos
+#Lertora como proyecto de producciones tipson.
 
 import pygame
 from random import *
@@ -9,19 +10,19 @@ from random import *
 pygame.init() #iniciar los modulos de pygame
 
 #-----------------------ventana principal----------------------------
-ventana=pygame.display.set_mode([700,400])
+ventana=pygame.display.set_mode([1000,600])
 pygame.display.set_caption("Priminsa (BETA)")
 fondo=(0,0,0)
 #--------------------------------------------------------------------
 
 #----------------------muros invisibles------------------------------
-murarriba=pygame.Rect(0,-500,700,500)
-murabajo=pygame.Rect(0,400,700,500)
+murarriba=pygame.Rect(0,-500,1000,500)
+murabajo=pygame.Rect(0,600,1000,500)
 #--------------------------------------------------------------------
 
 #-----------------------------player---------------------------------
-posx=325
-posy=175
+posx=625
+posy=375
 vx=0
 vy=0
 player=pygame.Rect(posx,posy,25,25)
@@ -31,13 +32,13 @@ modo=0
 #--------------------------------------------------------------------
 
 #------------------------------texto---------------------------------
-texto=pygame.font.Font(None,30)
+texto=pygame.font.Font(None,40)
 blanco=(255,255,255)
 inicio=texto.render("aplasta espacio para iniciar ",0,blanco)
 tipson=texto.render("#byproduccionestipson",0,blanco)
 textalmas=texto.render("ALMAS: ",0, blanco)
 textscore=texto.render("SCORE: ",0, blanco)
-over=pygame.font.Font(None,90)
+over=pygame.font.Font(None,120)
 gameo=over.render("GAME OVER",0,blanco)
 #--------------------------------------------------------------------
 
@@ -46,20 +47,20 @@ velocidad=-15#velocidad de los boques de la gran priminsa
 #---------------------------puntos-----------------------------------
 listpuntos=[]
 for x in range(1000):
-        ancho=randrange(10,50)
-        alto=randrange(10,50)
-        x=randrange(700,210000)
-        y=randrange(10,350)        
+        ancho=randrange(10,70)
+        alto=randrange(10,70)
+        x=randrange(1000,210000)
+        y=randrange(10,550)        
         listpuntos.append(pygame.Rect(x,y,ancho,alto))
 #--------------------------------------------------------------------
 
 #------------------------obstaculos----------------------------------
 listobstaculos=[]
 for x in range(3000):
-        ancho=randrange(10,50)
-        alto=randrange(10,50)
-        x=randrange(700,210000)
-        y=randrange(10,350)
+        ancho=randrange(10,70)
+        alto=randrange(10,70)
+        x=randrange(1000,210000)
+        y=randrange(10,550)
         listobstaculos.append(pygame.Rect(x,y,ancho,alto))
 #--------------------------------------------------------------------
 
@@ -68,8 +69,8 @@ listalmas=[]
 for x in range(10):
         ancho=randrange(30,90)
         alto=randrange(30,90)
-        x=randrange(700,210000)
-        y=randrange(10,350)
+        x=randrange(1000,210000)
+        y=randrange(10,550)
         listalmas.append(pygame.Rect(x,y,ancho,alto))
 #--------------------------------------------------------------------
 
@@ -83,22 +84,23 @@ while modo==0:
                 if event.type == pygame.KEYDOWN:#inicio
                         if event.key == pygame.K_SPACE:
                                 modo=1
-        ventana.blit(inicio,(220,50))
+        ventana.blit(inicio,(320,50))
+        reloj.tick(10)
         #-----------------------explicacion--------------------------
-        pygame.draw.rect(ventana,(0,200,0),(270,125,20,20))
+        pygame.draw.rect(ventana,(0,200,0),(370,180,20,20))
         textverde=texto.render("genera puntos",0,blanco)
-        ventana.blit(textverde,(300,125))
-        pygame.draw.rect(ventana,(200,0,0),(270,175,20,20))
+        ventana.blit(textverde,(400,175))
+        pygame.draw.rect(ventana,(200,0,0),(370,230,20,20))
         textrojo=texto.render("quita almas",0,blanco)
-        ventana.blit(textrojo,(300,175))
-        pygame.draw.rect(ventana,(255,255,255),(270,225,20,20))
+        ventana.blit(textrojo,(400,225))
+        pygame.draw.rect(ventana,(255,255,255),(370,280,20,20))
         textblanco=texto.render("genera almas (vida)",0,blanco)
-        ventana.blit(textblanco,(300,225))
+        ventana.blit(textblanco,(400,275))
         advertencia=texto.render("los filos te empujan",0,blanco)
-        #ventana.blit(advertencia,(275,275))
+        ventana.blit(advertencia,(375,325))
         #------------------------------------------------------------
 
-        ventana.blit(tipson,(470,370))
+        ventana.blit(tipson,(680,570))
 
         pygame.display.update()
 #--------------------------------------------------------------------
@@ -111,10 +113,10 @@ while modo == 1:#loop principal(el verdadero juego)
 
         #-----------------habilitar muros invisibles-----------------
         #los muros estan desactivados hasta nuevo aviso
-        """if player.colliderect(murarriba):
+        if player.colliderect(murarriba):
                 vy+=1
         if player.colliderect(murabajo):
-                vy-=1"""
+                vy-=1
         #------------------------------------------------------------
 
         #----------------------graficar texto------------------------
@@ -123,20 +125,19 @@ while modo == 1:#loop principal(el verdadero juego)
         contalmas=texto.render(strvida,0,blanco)
         contscore=texto.render(strscore,0,blanco)
         ventana.blit(textalmas,(50,20))
-        ventana.blit(contalmas,(150,20))
-        ventana.blit(textscore,(350,20))
-        ventana.blit(contscore,(450,20))
+        ventana.blit(contalmas,(160,20))
+        ventana.blit(textscore,(650,20))
+        ventana.blit(contscore,(760,20))
         #------------------------------------------------------------
-
 
         for event in pygame.event.get():
 
                 #---------------------controles----------------------
                 if event.type == pygame.KEYDOWN:
                         if event.key == pygame.K_RIGHT:
-                                vx+=15
+                                vx+=10
                         if event.key == pygame.K_LEFT:
-                                vx-=15
+                                vx-=10
                         if event.key == pygame.K_UP:
                                 vy-=15
                         if event.key == pygame.K_DOWN:
@@ -168,7 +169,7 @@ while modo == 1:#loop principal(el verdadero juego)
                 pygame.draw.rect(ventana,(200,0,0),obsta)
                 obsta.move_ip(velocidad,0)
                 if player.colliderect(obsta):
-                        obsta.move_ip(-700,0)
+                        obsta.move_ip(-1000,0)
                         vida -= 1
                         print "una vida menos"
                         print "te quedan ",vida," almas"
@@ -177,7 +178,7 @@ while modo == 1:#loop principal(el verdadero juego)
                 pygame.draw.rect(ventana,(0,200,0),punto)
                 punto.move_ip(velocidad,0)
                 if player.colliderect(punto):
-                        punto.move_ip(-700,0)
+                        punto.move_ip(-1000,0)
                         score += 10
                         print "score: ", score
 
@@ -185,7 +186,7 @@ while modo == 1:#loop principal(el verdadero juego)
                 pygame.draw.rect(ventana,(255,255,255),alma)
                 alma.move_ip(velocidad,0)
                 if player.colliderect(alma):
-                        alma.move_ip(-700,0)
+                        alma.move_ip(-1000,0)
                         vida += 1
                         print "una vida más"
                         print "ahora tienes ",vida," almas"
@@ -195,17 +196,25 @@ while modo == 1:#loop principal(el verdadero juego)
 
         if vida <= 0:#GAME OVER!!!!
                 print "GAME OVER!!!!"
-                modo=2
+                if score >= 8000:
+                        modo=10000
+                else:
+                        modo=2
 
         pygame.display.update()
 
 #-----------------------------DIOS-----------------------------------
 while modo==10000:
         dios=over.render("DIOS!!!",0,blanco)
-        ventana.blit(dios,(250,125))
+        ventana.blit(dios,(350,125))
+        strscore=str(score)
+        finalscore=over.render(strscore,0,blanco)
+        ventana.blit(finalscore,(450,275))
+        reloj.tick(10)
         for event in pygame.event.get():
                 if event.type==pygame.QUIT:
-                        print"""Padre nuestro,
+                        print"""
+                        Padre nuestro,
                         que estás en el cielo,
                         santificado sea tu Nombre;
                         venga a nosotros tu reino;
@@ -231,10 +240,11 @@ while modo==2:
                         print "tu score fue de ",score
                         modo+=1
 
-        ventana.blit(gameo,(150,125))
+        ventana.blit(gameo,(250,125))
         strscore=str(score)
         finalscore=over.render(strscore,0,blanco)
-        ventana.blit(finalscore,(300,300))
+        ventana.blit(finalscore,(450,300))
+        reloj.tick(10)
 
         pygame.display.update()
 pygame.quit()
